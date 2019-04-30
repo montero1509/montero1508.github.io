1b69352b2898a8c15d20a307cef002eca66a3ee7
montero1509 <47747642+montero1509@users.noreply.github.com>
2019-04-30 18:41:44 -0400

    Add files via upload

---
+++ final/form.js
@@ -0,0 +1,15 @@
+function validar()
+{
+var usuario=document.getElementById("usuario").value;
+var contraseña=document.getElementById("contraseña").value;
+
+if(usuario=="alan y anais" && contraseña =="ledesmacamacho")
+{
+    alert("Bienvenido") 
+}
+else
+{
+alert("Intentelo de nuevo");
+}
+
+}
